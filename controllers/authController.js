bbfb37cffaced000458c0f2934ae9d40538218ef
Jordane <takoukam.jordane@gmail.com>
2024-08-25 17:14:46 +0100

    update auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,22 +22,34 @@ exports.register = async (req, res) => {
 };
 
 // Connexion d'un utilisateur
+
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    // Rechercher l'utilisateur par email
     const user = await User.findOne({ email });
-    // retirer await pour la comparaison
-    if (!user || !(bcrypt.compare(password, user.password))) {
-      return res.status(400).json({ message: "Identifiants invalides" });
+
+    // Vérifier si l'utilisateur existe
+    if (!user) {
+      return res.status(400).json({ message: "Utilisateur non trouvé" });
     }
 
+    // Vérifier si le mot de passe est correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Mot de passe incorrect" });
+    }
+
+    // Générer un token JWT
     const token = jwt.sign({ id: user._id, email: email }, process.env.JWT_SECRET, {
       expiresIn: "24h",
     });
 
+    // Retourner le token
     res.status(200).json({ token });
   } catch (error) {
+    console.error("Erreur lors de la connexion:", error);
     res.status(500).json({ message: "Erreur lors de la connexion" });
   }
 };
